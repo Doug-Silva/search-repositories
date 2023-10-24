05fe1a1b5820662bcde3b10dfdc36e59f2f5e05d
Doug-Silva <silvatec.br@gmail.com>
2023-10-23 23:05:08 -0300

    Criando a estrutura central

--- src/pages/RepositoriesPage/index.jsx
+++ src/pages/RepositoriesPage/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
+import { Container, Sidebar, Main } from "./styles";
+
 function RepositoriesPage() {
-  return <h1>Repositories</h1>;
+  return (
+    <Container>
+      <Sidebar>Sidebar</Sidebar>
+      <Main>Main</Main>
+    </Container>
+  );
 }
 
 export default RepositoriesPage;
