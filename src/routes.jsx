a700f649e84afb391846edcbab2c51c80450127b
Doug-Silva <silvatec.br@gmail.com>
2023-10-23 22:38:33 -0300

    Criando a tela inicial

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 import MainPage from "./pages/MainPage";
+import RepositoriesPage from "./pages/RepositoriesPage";
 
 export function AppRoutes() {
   return (
     <Routes>
-      {/* /login/repositories */}
+      <Route path="/repositories" element={<RepositoriesPage />} />
       <Route path="/" element={<MainPage />} />
     </Routes>
   );
